fc1a1b958a94b1d71db23032ffd19df4f3d036d2
tojo <tobias.johansson@arccore.com>
2014-02-15 16:39:08 +0100

    Initial Commit

---
+++ midi/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  midi
+//
+//  Created by Tobias Johansson on 2014-02-15.
+//  Copyright (c) 2014 JONI. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
